ccb7ba394342293ce2552b60801b6dee049f28ec
Lakshay Malhotra <lakshaymalhotra17@yahoo.in>
2021-03-09 16:06:43 +0530

    added sign in and sign up pages

--- screens/signup.js
+++ screens/signup.js
@@ -4,7 +4,7 @@ import { globalStyles } from '../styles/global';
 
 
 
-export default function Login({ navigation }) {
+export default function Signup({ navigation }) {
     const [text, setText] = useState('');
     const changeHandler = (val) => {
         setText(val)
@@ -15,15 +15,24 @@ export default function Login({ navigation }) {
 
             <TextInput style={styles.textInputStyle}
                 placeholder='Enter your name'
-                onChangeText={(val) => changeHandler(val)}
+               
+            />
+            <TextInput style={styles.textInputStyle}
+                placeholder='Enter your Age'
+            />
+            
+             <TextInput style={styles.textInputStyle}
+                placeholder='Enter your Email-ID'
+           
             />
             <TextInput style={styles.textInputStyle}
                 placeholder='Enter your password'
-            // onChangeText={(val)=>changeHandler(val)//
             />
+
             <Button
                 onPress={() => navigation.navigate('Home', { screen: 'Home' })}
-                title='Login' color='coral'
+                title='Login' 
+                color='coral'
 
             />
 
@@ -34,11 +43,14 @@ export default function Login({ navigation }) {
 
 const styles = StyleSheet.create({
     textInputStyle: {
-        borderColor: '#9a73ef',
+        borderColor: 'black',
         borderWidth: 1,
-        height: 40,
-        margin: 20,
-        padding: 30,
+       marginBottom:10,
+       height:40,
+       marginTop:30,
+       paddingLeft:8
+       
+       
 
     }
 })
